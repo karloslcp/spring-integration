271d5fd672702a6f72161727e68f055f38148e6f
Karlos Chita <karloslcp@gmail.com>
2021-10-09 23:01:06 -0500

    Understanding channels

--- src/main/java/com/oci/springintegration/SpringIntegrationApplication.java
+++ src/main/java/com/oci/springintegration/SpringIntegrationApplication.java
@@ -3,18 +3,27 @@ package com.oci.springintegration;
 import java.util.Collections;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.integration.channel.DirectChannel;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.MessagingException;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.messaging.support.MessageBuilder;
 
 @SpringBootApplication
+@ImportResource("integration-context.xml")
 public class SpringIntegrationApplication implements ApplicationRunner
 {
+    @Autowired
+    private DirectChannel channel;
+
     public static void main(String[] args)
     {
         SpringApplication.run(SpringIntegrationApplication.class, args);
@@ -41,5 +50,22 @@ public class SpringIntegrationApplication implements ApplicationRunner
             .build();
 
         printService.print(message2);
+
+        // Creating a message - version 3
+        Message<String> message3 = MessageBuilder
+            .withPayload("Priviet, Worldu!")
+            .setHeader("Zdravstvui", "shto ti?")
+            .build();
+
+//        channel.subscribe((message) -> new PrintService().print((Message<String>) message));
+        channel.subscribe(new MessageHandler()
+        {
+            @Override
+            public void handleMessage(final Message<?> message) throws MessagingException
+            {
+                new PrintService().print((Message<String>) message);
+            }
+        });
+        channel.send(message3);
     }
 }
